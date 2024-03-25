613b9d8abb483b88d5a435e5c77fee93196a5614
Ofek Ashery <ofek.ashery@gmail.com>
2024-03-25 23:58:39 +0200

    Improve event style

--- src/components/event/event.tsx
+++ src/components/event/event.tsx
@@ -17,15 +17,17 @@ const Event = ({
     queryFn: () => getEventInfo(eventCode),
   });
   return (
-    <Card className="flex items-center p-2 justify-center gap-3 md:gap-10 flex-col grow">
-      <div className="flex flex-col gap-2 items-center md:items-start">
-        <div className="flex gap-4">
+    <Card className="flex flex-col grow">
+      <div className="flex flex-col gap-2 p-8 pb-4">
+        <div className="flex items-center gap-4">
           <div className="text-2xl font-bold">{event?.name}</div>
           <Indicator eventCode={eventCode} />
         </div>
         <EventSelector eventIndex={eventIndex} eventCode={eventCode} />
       </div>
-      <Fields eventCode={eventCode} />
+      <div className="flex grow justify-center items-center p-2 pb-8">
+        <Fields eventCode={eventCode} />
+      </div>
     </Card>
   );
 };
