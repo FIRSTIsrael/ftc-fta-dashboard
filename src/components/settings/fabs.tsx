853ebf616e429f840eb398890c0368b5e89b61c3
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 21:09:39 +0200

    WIP: fabs

---
+++ src/components/settings/fabs.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Expand from "@/assets/svgs/expand";
+import { Button } from "@/components/ui/button";
+import SettingsPage from "./settingsPage";
+import Logs from "./logs";
+
+const Fabs = () => {
+  return (
+    <div className="fixed bottom-4 right-4 flex gap-2">
+      <Logs />
+      <SettingsPage />
+      {/* TODO: use the button */}
+      <Button variant="outline" size="sm" disabled>
+        <Expand className=" fill-white w-3" />
+      </Button>
+    </div>
+  );
+};
+
+export default Fabs;
