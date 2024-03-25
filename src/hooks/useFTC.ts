adfa9415a3fc6cd08d3e35111dae781f6acfbc80
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 21:01:45 +0200

    WIP: create useFTC hook for websocket management

---
+++ src/hooks/useFTC.ts
@@ -0,0 +1,50 @@
+import { ENDPOINT } from "@/constants";
+import { getEventInfo } from "@/lib/ftcApi";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+const useFTC = (eventCode: string) => {
+  const { status: queryStatus } = useQuery({
+    queryKey: [eventCode, "info"],
+    queryFn: () => getEventInfo(eventCode),
+  });
+  const { lastMessage: _lastMessage, readyState } = useWebSocket(
+    `ws://${ENDPOINT}/stream/control/?code=${eventCode}`,
+    {
+      share: true,
+    }
+  );
+
+  const lastMessage = useMemo(() => {
+    if (!_lastMessage) return undefined;
+    return _lastMessage.data === "pong"
+      ? { type: "PONG" }
+      : JSON.parse(_lastMessage.data);
+  }, [_lastMessage]);
+
+  const status = useMemo<"Connected" | "Disconnected" | "Connecting">(() => {
+    if (readyState === ReadyState.OPEN && queryStatus === "success") {
+      return "Connected";
+    }
+    if (
+      readyState === ReadyState.CLOSED ||
+      readyState === ReadyState.CLOSING ||
+      readyState === ReadyState.UNINSTANTIATED ||
+      queryStatus === "error"
+    ) {
+      return "Disconnected";
+    }
+    if (readyState === ReadyState.CONNECTING || queryStatus === "pending") {
+      return "Connecting";
+    }
+    return "Disconnected";
+  }, [queryStatus, readyState]);
+
+  return {
+    lastMessage,
+    status,
+  };
+};
+
+export default useFTC;
