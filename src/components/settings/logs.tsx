f31ada797213a80fa28446ecb35474b18619af06
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 21:09:32 +0200

    WIP: logs page

---
+++ src/components/settings/logs.tsx
@@ -0,0 +1,16 @@
+import Terminal from "@/assets/svgs/terminal";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+const Logs = () => (
+  <Dialog>
+    <DialogTrigger asChild>
+      <Button variant="outline" size="sm">
+        <Terminal className=" fill-white w-4" />
+      </Button>
+    </DialogTrigger>
+    <DialogContent className="sm:max-w-[425px]">Logs goes here</DialogContent>
+  </Dialog>
+);
+
+export default Logs;
