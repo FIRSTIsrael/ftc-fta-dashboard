70110643e790a87b356c44250ec935b793a58a8b
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 23:38:38 +0200

    move indicator to be per event

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,15 +1,11 @@
 import FIRSTTechChallenge from "@/assets/svgs/firstTechChallenge";
 import { Card } from "@/components/ui/card";
 import Time from "./time";
-import Indicator from "./indicator";
 
 const Header = () => (
   <Card className="flex justify-between items-center p-6">
     <FIRSTTechChallenge className="w-[8rem]" />
-    <div className="flex items-center gap-4">
-      <Indicator />
-      <Time />
-    </div>
+    <Time />
   </Card>
 );
 
--- src/components/event/event.tsx
+++ src/components/event/event.tsx
@@ -3,6 +3,7 @@ import { Card } from "../ui/card";
 import Fields from "./fields";
 import { useQuery } from "@tanstack/react-query";
 import { getEventInfo } from "@/lib/ftcApi";
+import Indicator from "./indicator";
 
 const Event = ({
   eventIndex,
@@ -18,7 +19,10 @@ const Event = ({
   return (
     <Card className="flex items-center p-2 justify-center gap-3 md:gap-10 flex-col grow">
       <div className="flex flex-col gap-2 items-center md:items-start">
-        <div className="text-2xl font-bold">{event?.name}</div>
+        <div className="flex gap-4">
+          <div className="text-2xl font-bold">{event?.name}</div>
+          <Indicator eventCode={eventCode} />
+        </div>
         <EventSelector eventIndex={eventIndex} eventCode={eventCode} />
       </div>
       <Fields eventCode={eventCode} />
