0d2ff45aa36ad7490f25128f03c8f64069ef318f
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 21:10:04 +0200

    WIP: single field

---
+++ src/components/event/field.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import useCurrentFieldMatch from "@/hooks/useCurrentFieldMatch";
+import useFieldTimer from "@/hooks/useFieldTimer";
+import { cn } from "@/lib/classNames";
+import { MatchAlliance } from "@/Models/matchAlliance";
+import { Card } from "@/components/ui/card";
+import SevenSegment from "@/components/ui/sevenSegment";
+
+const Team = ({ number }: { number?: number }) => (
+  <SevenSegment enabled={!!number} value={String(number)} minimumLength={5} />
+);
+
+const Alliance = ({ alliance }: { alliance?: MatchAlliance }) => (
+  <div className="flex flex-col justify-center gap-12">
+    <Team number={alliance?.team1} />
+    <Team number={alliance?.team2} />
+  </div>
+);
+
+const MatchTimer = ({
+  fieldTimer: {
+    fieldTimer: { minutes, seconds },
+    synced,
+  },
+}: {
+  fieldTimer: ReturnType<typeof useFieldTimer>;
+}) => (
+  <div className="flex w-fit">
+    <SevenSegment
+      enabled={synced}
+      value={String(minutes ?? 0).padStart(2, "0")}
+      minimumLength={2}
+    />
+    <SevenSegment enabled value=":" fixedSize={false} />
+    <SevenSegment
+      enabled={synced}
+      value={String(seconds ?? 0).padStart(2, "0")}
+      minimumLength={2}
+    />
+  </div>
+);
+
+const Field = ({ field }: { field: number }) => {
+  //TODO: Make it not a const.
+  const { currentMatch, blueReady, redReady, fieldStatus, matchStartTime } =
+    useCurrentFieldMatch("ilcmp_1", field);
+  const fieldTimer = useFieldTimer(matchStartTime ?? 0);
+
+  return (
+    <div className="flex gap-2">
+      <Alliance alliance={currentMatch?.blue!} />
+      <Card
+        className={cn(
+          "aspect-square w-40 bg-gradient-to-r from-transparent via-transparent p-1 rounded-xl",
+          {
+            "animate-fast-red-flash": fieldStatus === "Aborted",
+            "animate-slow-green-flash": fieldStatus === "In-Game",
+            "to-red-500": redReady && fieldStatus !== "In-Game",
+            "from-blue-500": blueReady && fieldStatus !== "In-Game",
+          }
+        )}
+      >
+        <Card className="h-full w-full flex flex-col gap-1 justify-center items-center">
+          <div>Field {field}</div>
+          <div>{fieldStatus}</div>
+          <MatchTimer fieldTimer={fieldTimer} />
+        </Card>
+      </Card>
+      <Alliance alliance={currentMatch?.red} />
+    </div>
+  );
+};
+
+export default Field;
