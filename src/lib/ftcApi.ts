4a0a4efc9a021a237f1e0ed9655be66e587c92f8
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 20:15:10 +0200

    add ftc api calls util

---
+++ src/lib/ftcApi.ts
@@ -0,0 +1,22 @@
+import { ENDPOINT } from "@/constants";
+import { Event } from "@/Models/event";
+import { Match } from "@/Models/match";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `http://${ENDPOINT}/api/v1/`,
+});
+
+const get = async (url: string, innerObjectName?: string) => {
+  const response = await instance.get(url);
+  return !!innerObjectName ? response.data[innerObjectName] : response.data;
+};
+
+export const getEvents = async (): Promise<string[]> =>
+  get("events/", "eventCodes");
+
+export const getEventInfo = async (eventCode: string): Promise<Event> =>
+  get(`events/${eventCode}/`);
+
+export const getMatches = async (eventCode: string): Promise<Match[]> =>
+  get(`events/${eventCode}/matches/`, "matches");
