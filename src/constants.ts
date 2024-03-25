556fc7daa5a9152dfd08b85cfa78cc7fa78bfa31
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-25 20:59:37 +0200

    create constants file

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const SEVEN_SEGMENT_DEFAULT_CHARACTER = "8";
+export const ENDPOINT = "localhost";
+export const DEFAULT_FIELDS_COUNT = 2;
+
+export const MILLISECONDS_IN_SECOND = 1000;
+
+export const GAME_AUTONOMOUS_LENGTH_SECONDS = 30;
+export const GAME_TRANSITION_LENGTH_SECONDS = 8;
+export const GAME_TELEOP_LENGTH_SECONDS = 120;
+export const GAME_LENGTH_SECONDS =
+  GAME_AUTONOMOUS_LENGTH_SECONDS +
+  GAME_TRANSITION_LENGTH_SECONDS +
+  GAME_TELEOP_LENGTH_SECONDS +
+  // Add one second to include the first full second
+  1;
