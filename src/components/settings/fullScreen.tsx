a0ff88bc9a321abcac05b47dee0eed39c3bf915e
Ofek Ashery <ofek.ashery@gmail.com>
2024-03-26 23:51:51 +0200

    Improve full screen button

--- src/components/settings/fullScreen.tsx
+++ src/components/settings/fullScreen.tsx
@@ -1,18 +1,41 @@
 "use client";
 
-import Expand from "@/assets/svgs/expand";
-import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import { MaximizeIcon, MinimizeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
-const FullScreen = () => (
-  <Button
-    variant="outline"
-    size="sm"
-    onClick={() => {
-      document.body.requestFullscreen();
-    }}
-  >
-    <Expand className="fill-current w-3" />
-  </Button>
-);
+const FullScreen = () => {
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleFullScreenChange = () => {
+      setIsFullScreen(!!document.fullscreenElement);
+    };
+    document.addEventListener("fullscreenchange", handleFullScreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullScreenChange);
+    };
+  }, []);
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => {
+        if (isFullScreen) {
+          document.exitFullscreen();
+        } else {
+          document.body.requestFullscreen();
+        }
+      }}
+    >
+      {isFullScreen ? (
+        <MinimizeIcon className="w-4" />
+      ) : (
+        <MaximizeIcon className="w-4" />
+      )}
+    </Button>
+  );
+};
 
 export default FullScreen;
