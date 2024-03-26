96bf850111490716b09c749fc03b4e4667a25be7
Shahar Ilany <31574996+ShaharIlany@users.noreply.github.com>
2024-03-26 22:50:01 +0200

    make full screen work

--- src/components/settings/fabs.tsx
+++ src/components/settings/fabs.tsx
@@ -5,17 +5,15 @@ import { Button } from "@/components/ui/button";
 import SettingsPage from "./settingsPage";
 import Logs from "./logs";
 import ThemeToggle from "./themeToggle";
+import FullScreen from "./fullScreen";
 
 const Fabs = () => {
   return (
     <div className="fixed bottom-4 right-4 flex gap-2">
       <Logs />
       <SettingsPage />
-      {/* TODO: use the button */}
       <ThemeToggle />
-      <Button variant="outline" size="sm" disabled>
-        <Expand className=" fill-current w-3" />
-      </Button>
+      <FullScreen />
     </div>
   );
 };
